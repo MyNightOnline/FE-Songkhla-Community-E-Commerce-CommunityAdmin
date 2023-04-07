e29e8c074ac068b01e629a5b73ec4460e0c67b8a
Radis Lamsuneekan <radisworldclassic.gm@gmail.com>
2023-04-07 15:48:42 +0700

    Start changing manage Order

--- src/layouts/default/Navbar.vue
+++ src/layouts/default/Navbar.vue
@@ -1,35 +1,7 @@
 <script setup lang="ts">
 import { useUserStore } from "@/store/index"
-import { onMounted } from 'vue'
-import {
-    initAccordions,
-    initCarousels,
-    initCollapses,
-    initDials,
-    initDismisses,
-    initDrawers,
-    initDropdowns,
-    initModals,
-    initPopovers,
-    initTabs,
-    initTooltips
-} from 'flowbite'
-const userStore = useUserStore()
-// initialize components based on data attribute selectors
-onMounted(() => {
-    initAccordions()
-    initCarousels()
-    initCollapses()
-    initDials()
-    initDismisses()
-    initDrawers()
-    initDropdowns()
-    initModals()
-    initPopovers()
-    initTabs()
-    initTooltips()
-})
 
+const userStore = useUserStore()
 const logout = () => {
     userStore.signOut()
 }
@@ -100,7 +72,7 @@ export default {
                     <li v-for="{ name, to } in menus">
                         <RouterLink :to="to"
                             class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            :class="($route.path == to) ? 'bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500' : ''">
+                            :class="($route.path == to) ? 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' : ''">
                             {{ name }}
                         </RouterLink>
                     </li>
--- src/views/ShopViews/ManageOrder.vue
+++ src/views/ShopViews/ManageOrder.vue
@@ -1,12 +1,114 @@
+<script setup>
+import Table from '@/components/ManageOrder/Table.vue'
+
+import { onMounted } from 'vue'
+import { initTabs } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initTabs()
+})
+
+const tabs = [
+    { name: 'ทั้งหมด', id: 'all-tab', target: 'all', text: 'TAB 1' },
+    { name: 'ชำระเงินแล้ว', id: 'paid-tab', target: 'paid', text: 'TAB 2' },
+    { name: 'ยังไม่ชำระเงิน', id: 'unpaid-tab', target: 'alunpaidl', text: 'TAB 3' },
+    { name: 'เตรียมผลิตภัณฑ์', id: 'prepare-the-product-tab', target: 'prepare', text: 'TAB 4' },
+    { name: 'จัดส่งผลิตภัณฑ์', id: 'product-delivery-tab', target: 'product', text: 'TAB 5' },
+]
+
+</script>
+
 <template>
     <div class="max-w-5xl container mx-auto mt-12">
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <Table />
         </div>
+        <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+            <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent"
+                role="tablist">
+                <li v-for="{ name, id, target } in tabs" class="mr-2" role="presentation">
+                    <button class="inline-block p-4 border-b-2 rounded-t-lg" :id="id" :data-tabs-target="`#${target}`"
+                        type="button" role="tab" :aria-controls="target" aria-selected="false">
+                        {{ name }} <strong style="color:red">10</strong>
+                    </button>
+                </li>
+            </ul>
+        </div>
+        <div id="myTabContent">
+            <div v-for="{ name, id, target, text } in tabs" class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
+                :id="target" role="tabpanel" :aria-labelledby="id">
+                {{ text }}
 
+                <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Product name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Color
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Category
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Price
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td class="px-6 py-4">
+                                    Silver
+                                </td>
+                                <td class="px-6 py-4">
+                                    Laptop
+                                </td>
+                                <td class="px-6 py-4">
+                                    $2999
+                                </td>
+                            </tr>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Microsoft Surface Pro
+                                </th>
+                                <td class="px-6 py-4">
+                                    White
+                                </td>
+                                <td class="px-6 py-4">
+                                    Laptop PC
+                                </td>
+                                <td class="px-6 py-4">
+                                    $1999
+                                </td>
+                            </tr>
+                            <tr class="bg-white dark:bg-gray-800">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Magic Mouse 2
+                                </th>
+                                <td class="px-6 py-4">
+                                    Black
+                                </td>
+                                <td class="px-6 py-4">
+                                    Accessories
+                                </td>
+                                <td class="px-6 py-4">
+                                    $99
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
-
-<script setup>
-import Table from '@/components/ManageOrder/Table.vue'
-</script>
